e012dbf1dc7861c3c81519154395d65906ac1c61
josepalaci0 <jose.palacio@esap.edu.co>
2022-02-05 21:39:00 -0500

    Update Login.js

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
-
+/*Login */
 const Login = (props) => {
   const { register, handleSubmit } = useForm()
   const [loginErr, setLoginErr] = useState("");
